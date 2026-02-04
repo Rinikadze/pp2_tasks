6a02c36d51720f75ecf16d6944c49c4462783383
Rinikadze <07andreik@gmail.com>
2026-02-04 23:46:59 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

--- Prac02/if_else/short_hand_if.py
+++ Prac02/if_else/short_hand_if.py
@@ -6,3 +6,12 @@ a = 10
 b = 20
 bigger = a if a > b else b
 print("Bigger is", bigger)
+
+x = 15
+y = 20
+max_value = x if x > y else y
+print("Maximum value:", max_value)
+
+username = ""
+display_name = username if username else "Guest"
+print("Welcome,", display_name)
