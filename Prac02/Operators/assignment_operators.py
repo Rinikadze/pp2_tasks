cc3be5eb76a5c3ff786a0a125c5bf7fe4137c083
Rinikadze <07andreik@gmail.com>
2026-02-04 21:57:32 +0500

    second commit

---
+++ Prac02/Operators/assignment_operators.py
@@ -0,0 +1,30 @@
+x = 5	
+print(x)
+x += 3
+print(x)
+x -= 3	
+print(x)
+x *= 3	
+print(x)
+x /= 3	
+print(x)
+x %= 3
+print(x)
+x //= 3
+print(x)
+x **= 3	
+print(x)
+
+x = 5
+x &= 3
+print(x)
+x |= 3	
+print(x)
+x ^= 3	
+print(x)
+x >>= 3	
+print(x)
+x <<= 3
+print(x)
+print(y := 3)
+print(y)
