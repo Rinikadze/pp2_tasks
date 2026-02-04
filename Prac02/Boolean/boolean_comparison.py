c3c7e4a3988e96d324f913c5de5e6bea1ab53649
Rinikadze <07andreik@gmail.com>
2026-02-04 22:31:53 +0500

    reworked commit

---
+++ Prac02/Boolean/boolean_comparison.py
@@ -0,0 +1,14 @@
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+print(1 < x < 10)
+print(1 < x and x < 10)
+print(x < 5 or x > 10)
+print(x > 0 and x < 10)
+print(not(x > 3 and x < 10))
