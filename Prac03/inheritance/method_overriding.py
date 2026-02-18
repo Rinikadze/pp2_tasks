5fa2a1dff7a7c9308ceef042951b632db0dd4055
Rinikadze <07andreik@gmail.com>
2026-02-19 03:00:34 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ Prac03/inheritance/method_overriding.py
@@ -0,0 +1,17 @@
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+x = Person("John", "Doe")
+x.printname()
+x = Student("Mike", "Olsen")
+x.printname()
