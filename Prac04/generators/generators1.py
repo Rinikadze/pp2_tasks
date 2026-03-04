665d0d3b7ae748722e30d122ed17b1949baf920e
Rinikadze <07andreik@gmail.com>
2026-03-05 02:27:16 +0500

    generators comm

--- Prac04/generators/generators1.py
+++ Prac04/generators/generators1.py
@@ -1,7 +1,7 @@
-def square_generator(n):
+def square(n):
     for i in range(n + 1):
         yield i * i
 
 n = int(input())
-for i in square_generator(n):
+for i in square(n):
     print(i)
---
+++ Prac04/generators/generators5.py
@@ -0,0 +1,8 @@
+def num(n):
+    while n >= 0:
+        yield n
+        n -= 1
+
+n = int(input())
+for i in num(n):
+    print(i)
